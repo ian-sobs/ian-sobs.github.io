49a6145e51608353dd43aac3a973cd3f800d428c
Ian Sobrecaray <ian.sobrecaray@gmail.com>
2026-09-11 00:11:27 +0800

    laflaf

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,4 @@
-import type { NextConfig } from "next"
-
-const nextConfig: NextConfig = {
+const nextConfig = {
     basePath: "/portfolio",
     output: "export",  // <=== enables static exports
     reactStrictMode: true,

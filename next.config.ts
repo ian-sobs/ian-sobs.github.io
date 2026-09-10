78ce56a8eb7096bb812ada6fb87d569321e6aa12
Ian Sobrecaray <ian.sobrecaray@gmail.com>
2026-09-10 23:38:55 +0800

    configured github actions and nextjs for github pages

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
+    basePath: "/portfolio",
     output: "export",  // <=== enables static exports
     reactStrictMode: true,
 }

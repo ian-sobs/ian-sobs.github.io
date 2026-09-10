f9d2da74151c11384fba3d623412cbbb88331857
Ian Sobrecaray <ian.sobrecaray@gmail.com>
2026-09-10 23:25:12 +0800

    github pages preparation

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,8 @@
 import type { NextConfig } from "next"
 
-const nextConfig: NextConfig = {}
+const nextConfig: NextConfig = {
+    output: "export",  // <=== enables static exports
+    reactStrictMode: true,
+}
 
 export default nextConfig
